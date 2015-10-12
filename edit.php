433537f18e0481a1645f0c400cc9c1cbf4d1bed2
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-10-12 13:28:28 +0300

    saving through edit functions

--- edit.php
+++ edit.php
@@ -1,6 +1,12 @@
 <?php
     require_once("edit_functions.php");
      
+    //Kasutaja muudab andmeid
+    if(isset($_GET["update"])){
+        //auto id, auto number, auto värv
+        updateCarData($_GET["car_id"], $_GET["number_plate"], $_GET["color"]);
+    }
+     
     //kas muutuja on aadressireal
     if(isset($_GET["edit_id"])){
         //trükin aadressirealt muutuja
@@ -18,10 +24,13 @@
         header("Location: table.php");
         
     }
+    
+    
+    
 
 ?>
 <!-- Salvestamiseks kasutan table.php rida 15, updateCar() -->
-<form action="table.php" method="get" >
+<form action="edit.php" method="get" >
     <input name="car_id" type="hidden" value="<?=$_GET["edit_id"];?>">
     <input name="number_plate" type="text" value="<?=$car->number_plate;?>" ><br>
     <input name="color" type="text" value="<?=$car->color;?>"><br>
